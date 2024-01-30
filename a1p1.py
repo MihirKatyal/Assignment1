3a7fc98f2441fc6c72462dd610ef7d660c65020d
Mihir Katyal <mkatyal@uci.edu>
2024-01-29 17:59:10 -0800

    updated def list_directory

--- a1p1.py
+++ a1p1.py
@@ -1,24 +1,11 @@
-
+from pathlib import Path
 
 def list_directory(path, options):
     files = []
-    for root, dirs, filenames in os.walk(path):
-        if '-f' in options:
-            # Output only files
-            files.extend([os.path.join(root, filename) for filename in filenames])
-        else:
-            # Output files and directories
-            files.extend([os.path.join(root, filename) for filename in filenames])
-            if '-r' not in options:
-                break  # If not recursive, only list the current directory
+    for file_path in Path(path).rglob("*"):
+        if file_path.is_file():
+            files.append(file_path)
 
-    if '-s' in options:
-        # Filter by file name
-        files = [file for file in files if options['-s'] in os.path.basename(file)]
-
-    if '-e' in options:
-        # Filter by file extension
-        files = [file for file in files if file.endswith(options['-e'])]
 
     files.sort(key=lambda x: (os.path.isdir(x), x))  # Sort files first, then directories
 
